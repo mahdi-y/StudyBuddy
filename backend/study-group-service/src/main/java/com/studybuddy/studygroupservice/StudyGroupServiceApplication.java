f9700f1256828a012fbebe34778c048f7e2a2233
mahdi-y <mohamedmahdi.zalila@esprit.tn>
2025-03-28 08:33:34 +0100

    Initial commit - Project structure setup

---
+++ backend/study-group-service/src/main/java/com/studybuddy/studygroupservice/StudyGroupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.studybuddy.studygroupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGroupServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGroupServiceApplication.class, args);
+    }
+
+}
