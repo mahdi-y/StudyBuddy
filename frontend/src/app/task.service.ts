5038a45bb2e183c2c654634e3be1a43228fdb610
mahdi-mmz <mohamedmahdi.zalila@esprit.tn>
2025-05-12 14:05:06 +0100

    Testing frontend-user deployment 14

--- frontend/src/app/task.service.ts
+++ frontend/src/app/task.service.ts
@@ -13,7 +13,7 @@ export class TaskService {
   constructor(private http: HttpClient) {}
 
   // Create a task
-  // task.service.tsbhgdddddfffffff
+  // task.service.tsbhgdddddffffffffff
   createTask(task: any): Observable<Task> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
